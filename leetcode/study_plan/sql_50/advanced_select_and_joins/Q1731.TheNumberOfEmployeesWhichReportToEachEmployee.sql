114c6980dea90520c5e14099ff0db6529f9b4dc1
LuisAxel <axelnunez5@gmail.com>
2024-06-25 15:55:57 -0600

    FEAT: sorting & grouping and advanced select & joins
    - Added accepted solutions for last 4 problems in sorting and grouping
    - Added accepted solution for first problem in advanced select and joins

---
+++ leetcode/study_plan/sql_50/advanced_select_and_joins/Q1731.TheNumberOfEmployeesWhichReportToEachEmployee.sql
@@ -0,0 +1,7 @@
+select m.employee_id, m.name,
+  count(*) as reports_count,
+  round(avg(e.age),0) as average_age
+from Employees m join
+  Employees e on m.employee_id = e.reports_to
+group by m.employee_id, m.name
+order by 1;

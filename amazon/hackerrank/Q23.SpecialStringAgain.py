f7681b4704bcd45f38bea737453dcfaf2ad506cc
LuisAxel <axelnunez5@gmail.com>
2025-01-25 01:03:44 -0600

    FIX: Amazon Hackerrank solution 13
    Fixed old solution

--- amazon/hackerrank/Q23.SpecialStringAgain.py
+++ amazon/hackerrank/Q23.SpecialStringAgain.py
@@ -8,20 +8,26 @@ import sys
 
 # Complete the substrCount function below.
 def substrCount(n, s):
-    count = n
-    for i in range(n):
-        diff_idx = -1
-        for j in range(i + 1, n):
-            if s[i] == s[j]:
-                if diff_idx == -1:
-                    count += 1
-                elif j - diff_idx == diff_idx - i:
-                    count += 1
-                    break
-            else:
-                if diff_idx != -1:
-                    break
-                diff_idx = j
+    count = 0
+    i = 0
+    while i < n:
+        same_char_count = 1
+        j = i + 1
+        while j < n and s[i] == s[j]:
+            j += 1
+            same_char_count += 1
+
+        count += same_char_count
+
+        odd_one = j
+        j += 1
+        while j < n and s[i] == s[j]:
+            if j - odd_one == odd_one - i:
+                count += 1
+                break
+            j += 1
+
+        i += 1
 
     return count
 
